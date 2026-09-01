f63717274fae12b52f7248a7a49d3f749cd25bf6
Abdirazak23 <abdirazaksharif79@gmail.com>
2026-09-01 14:03:20 +0100

    hub vnet/subent setup

--- main.tf
+++ main.tf
@@ -1,1 +1,50 @@
-a
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=5.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "hub-rg" {
+  name     = "HubVnet-rg"
+  location = "uk south"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.hub-rg.location
+  resource_group_name = azurerm_resource_group.hub-rg.name
+  address_space       = ["10.0.0.0/16"]
+
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.hub-rg.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.hub-rg.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+
+  delegation {
+    name = "gateway-delegation"
+
+    service_delegation {
+      name    = "Microsoft.gateway/service"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+}
+
