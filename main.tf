fc11767bc9ccdec60c26318b9c418c87e526ba20
Abdirazak23 <abdirazaksharif79@gmail.com>
2026-09-04 11:03:22 +0100

    spoke added

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider "azurerm" {
 
 module "hub_network" {
   source = "git::https://github.com/Abdirazak23/Central-library.git//Terraform-Modules/Modules/hub-vnet?ref=main"
+  source = "git::https://github.com/Abdirazak23/Central-library.git//Terraform-Modules/Modules/spoke-vnet?ref=main"
 
   hub-vnet-rg              = var.hub-vnet-rg
   location                 = var.location
@@ -23,4 +24,9 @@ module "hub_network" {
   gateway_subnet_prefix    = var.gateway_subnet_prefix
   firewall_subnet_prefix   = var.firewall_subnet_prefix
   management_subnet_prefix = var.management_subnet_prefix
+  spoke-vnet-rg            = var.spoke-vnet-rg
+  vnet_name_spoke          = var.vnet_name_spoke
+  address_space_spoke      = var.address_space_spoke
+  Subnet01_prefix          = var.Subnet01_prefix
+  Subnet02_prefix          = var.Subnet02_prefix
 }
--- terraform.tfvars
+++ terraform.tfvars
@@ -7,3 +7,11 @@ address_space            = ["10.0.0.0/16"]
 gateway_subnet_prefix    = ["10.0.0.32/27"]
 firewall_subnet_prefix   = ["10.0.0.64/27"]
 management_subnet_prefix = ["10.0.0.96/27"]
+
+#spoke vars
+
+spoke-vnet-rg            = "Vnet"
+vnet_name_spoke          = "vnet"
+address_space_spoke      = ["10.1.0.0/16"]
+Subnet01_prefix          = ["10.1.0.32/27"]
+Subnet02_prefix          = ["10.1.0.64/27"]
