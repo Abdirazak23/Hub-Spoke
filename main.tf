23dee0b0a9edec52d8a5a23bfc17dafa8049f231
Abdirazak23 <abdirazaksharif79@gmail.com>
2026-09-04 11:07:05 +0100

    seprate module for spoke as cannot calls 2 sources in 1 module

--- main.tf
+++ main.tf
@@ -13,7 +13,6 @@ provider "azurerm" {
 
 module "hub_network" {
   source = "git::https://github.com/Abdirazak23/Central-library.git//Terraform-Modules/Modules/hub-vnet?ref=main"
-  source = "git::https://github.com/Abdirazak23/Central-library.git//Terraform-Modules/Modules/spoke-vnet?ref=main"
 
   hub-vnet-rg              = var.hub-vnet-rg
   location                 = var.location
@@ -24,9 +23,16 @@ module "hub_network" {
   gateway_subnet_prefix    = var.gateway_subnet_prefix
   firewall_subnet_prefix   = var.firewall_subnet_prefix
   management_subnet_prefix = var.management_subnet_prefix
+
+}
+
+module "spoke_network" {
+  source = "git::https://github.com/Abdirazak23/Central-library.git//Terraform-Modules/Modules/spoke-vnet?ref=main"
+  
   spoke-vnet-rg            = var.spoke-vnet-rg
   vnet_name_spoke          = var.vnet_name_spoke
   address_space_spoke      = var.address_space_spoke
   Subnet01_prefix          = var.Subnet01_prefix
   Subnet02_prefix          = var.Subnet02_prefix
+
 }
